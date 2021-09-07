ccea702ce9e15601fc9fe14647dc0a19e103e07a
Konstantin <diemydiesel@gmail.com>
2021-09-07 20:02:12 +0300

    fixed json users

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -4,7 +4,7 @@ from api.models.user import UserModel
 class UserSchema(ma.SQLAlchemyAutoSchema):
    class Meta:
        model = UserModel
-
+       exclude = ["password_hash"] #чтобы при get запросе не выводился хэш-пароль пользователей
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
